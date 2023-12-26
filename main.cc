0b1d8f95d663ba0251f123121aa6456b6da5dddf
Alex_A <77032042+crazininja@users.noreply.github.com>
2023-12-26 16:55:18 -0500

    chapter 5 complete

--- main.cc
+++ main.cc
@@ -1,15 +1,27 @@
 #include "color.h"
-#include "vec3.h"
 #include "ray.h"
+#include "vec3.h"
 
 #include <iostream>
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+	vec3 oc = r.origin() - center;
+	auto a = dot(r.direction(), r.direction());
+	auto b = 2.0 * dot(oc, r.direction());
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = b * b - 4 * a * c;
+	return (discriminant >= 0);
+}
+
+
 color ray_color(const ray& r) {
+	if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+		return color(1, 0, 0);
+	}
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
     return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
-
 
 
 int main() {
@@ -23,7 +35,6 @@ int main() {
 	image_height = (image_height < 1) ?  1 : image_height;
 	
 	//Camera
-
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
 	auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
@@ -31,7 +42,7 @@ int main() {
 
 	//calculate the	vectors across the horizontal and vertical viewport edges
 	auto viewport_u = vec3(viewport_width, 0, 0);
-	auto viewport_y = vec3(0, -viewport_height 0);
+	auto viewport_v= vec3(0, -viewport_height, 0);
 
 	//calculate the horizontal and vertical delta vectors from pixel to pixel
 	auto pixel_delta_u = viewport_u / image_width;
@@ -56,5 +67,6 @@ int main() {
 			write_color(std::cout, pixel_color);
 		}
 	}
+
 	std::clog << "\rDone.                 \n";
 }
