f5faab3528bb767b5e7b29ed48584992170d0a20
Alex_A <77032042+crazininja@users.noreply.github.com>
2023-12-28 00:20:31 -0500

    chapter 6 finished

--- sphere.h
+++ sphere.h
@@ -1,14 +1,13 @@
 #ifndef SPHERE_H
 #define SPHERE_H
 
-#include "hittable.h"
-#include "vec3.h"
+#include "rtweekend.h"
 
 class sphere : public hittable {
 	public:
 		sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
 		
-		bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+		bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
 			vec3 oc = r.origin() - center;
 			auto a = r.direction().length_squared();
 			auto half_b = dot(oc, r.direction());
@@ -16,13 +15,13 @@ class sphere : public hittable {
 
 			auto discriminant = half_b * half_b - a * c;
 			if (discriminant < 0) return false;
-			auto sqrtd = sqrt(discriminant)
+			auto sqrtd = sqrt(discriminant);
 
 			//find the nearest root that lies in the acceptable range
 			auto root = (-half_b - sqrtd) / a;
-			if (root <= ray_tmin || ray_tmax <= root) {
+			if (!ray_t.surrounds(root)) {
 				root = (-half_b + sqrtd) / a;
-				if (root <= ray_tmin || ray_tmax <= root) {
+				if (!ray_t.surrounds(root)) {
 					return false;
 				}
 			}
